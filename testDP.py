625fd9856771deb21b7dbc93e6cc86d7ba3138d0
wang minghui <wmh123456789@qq.com>
2021-07-24 20:59:30 +0800

    try DP of adder problem

--- testDP.py
+++ testDP.py
@@ -1,23 +1,22 @@
 
-
-
-
-def find_adder(sum, num):
+def find_adder(sum, num, min=1):
     rst = []
     if num == 1:
-        return [[sum]]
+        if sum >= min:
+            return [[sum]]
     elif num > 1:
-        for i in range(1,num):
-            return [[i]+sub_rst for sub_rst in find_adder(sum-i, num-1)]
+        for i in range(min, sum):
+            rst += [[i] + sub_rst for sub_rst in find_adder(sum - i, num - 1, i) if sub_rst]
 
-    pass
-
-
-
+    return rst
 
 
 
 def main():
+    sum = 20
+    num = 3
+    rst = find_adder(sum, num)
+    print(f"For {num} adders get {sum}, found {len(rst)} ways:\n {rst}")
 
 
     pass
