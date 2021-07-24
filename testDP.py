e3734d8d5e2b29d233755d7607bfc9f1492800a8
wang minghui <wmh123456789@qq.com>
2021-07-24 21:25:52 +0800

    try DP of adder problem

--- testDP.py
+++ testDP.py
@@ -1,24 +1,40 @@
 
-def find_adder(sum, num, min=1):
-    rst = []
-    if num == 1:
-        if sum >= min:
-            return [[sum]]
-    elif num > 1:
-        for i in range(min, sum):
-            rst += [[i] + sub_rst for sub_rst in find_adder(sum - i, num - 1, i) if sub_rst]
+class DPFindAdder:
+    dp = {}
+    call_cnt = 0
 
-    return rst
+    def __init__(self):
+        pass
+
+    def find_adder(self,sum, num, min=1):
+        self.call_cnt += 1
+        rst = []
+        if num == 1:
+            if sum >= min:
+                return [[sum]]
+        elif num > 1:
+            for i in range(min, sum):
+                if (sum-i, num-1, i) in self.dp:
+                    rst += [[i] + sub_rst for sub_rst in self.dp[(sum-i, num-1, i)]]
+                else:
+                    rst += [[i] + sub_rst for sub_rst in self.find_adder(sum - i, num - 1, i) if sub_rst]
+
+        self.dp.update({(sum, num, min): rst})
+        return rst
 
 
 
 def main():
-    sum = 20
-    num = 3
-    rst = find_adder(sum, num)
-    print(f"For {num} adders get {sum}, found {len(rst)} ways:\n {rst}")
-
-
+    adder_sum = 100
+    adder_num = 10
+    adder_min = 1
+    dp = DPFindAdder()
+    rst = dp.find_adder(adder_sum, adder_num, adder_min)
+    print(f"For {adder_num} adders get {adder_sum}, found {len(rst)} ways")
+    if len(rst)<100:
+        print(rst)
+    else:
+        print(f"with {dp.call_cnt} calls")
     pass
 
 
